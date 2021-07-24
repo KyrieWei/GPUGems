53ebda1779818f620163eb7bad9587dd835b4ed9
unknown <weiwenguo@ztgame.com>
2021-07-24 16:34:26 +0800

    implement shadow mapping

--- GPUGems/shadowMapping/shadowMapping.cpp
+++ GPUGems/shadowMapping/shadowMapping.cpp
@@ -12,20 +12,21 @@ void show_shadow_mapping(GLFWwindow* window, Camera& camera, unsigned int SCR_WI
 
 #pragma region floor
 
-    GLint floor_vertex_stride = 5;
+    const GLint floor_vertex_stride = 8;
 
-    float floor[4 * 5] =
+    float floor[4 * floor_vertex_stride] =
     {
-        -5.0f, 0.0f, -5.0f, 0.0f, 0.0f,
-         5.0f, 0.0f, -5.0f, 5.0f, 0.0f,
-         5.0f, 0.0f,  5.0f, 5.0f, 5.0f,
-        -5.0f, 0.0f,  5.0f, 0.0f, 5.0f
+         //vertex                  //normal             //texcoord
+        -25.0f, 0.0f, -25.0f,     0.0f, 1.0f, 0.0f,    0.0f,  0.0f,
+         25.0f, 0.0f, -25.0f,     0.0f, 1.0f, 0.0f,    25.0f, 0.0f,
+         25.0f, 0.0f,  25.0f,     0.0f, 1.0f, 0.0f,    25.0f, 25.0f,
+        -25.0f, 0.0f,  25.0f,     0.0f, 1.0f, 0.0f,    0.0f,  25.0f,
     };
 
     unsigned int floor_index[6] =
     {
-        0, 1, 2,
-        0, 2, 3
+        0, 2, 1,
+        0, 3, 2
     };
 
     GLuint floor_VAO, floor_VBO, floor_EBO;
@@ -44,8 +45,11 @@ void show_shadow_mapping(GLFWwindow* window, Camera& camera, unsigned int SCR_WI
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, floor_vertex_stride * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, floor_vertex_stride * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, floor_vertex_stride * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, floor_vertex_stride * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
     glBindVertexArray(0);
 
@@ -73,23 +77,154 @@ void show_shadow_mapping(GLFWwindow* window, Camera& camera, unsigned int SCR_WI
 
     stbi_image_free(floor_texture_data);
 
-    Shader floor_shader("shadowMapping/shaders/floor_vert.vs", "shadowMapping/shaders/floor_frag.fs");
-
-    floor_shader.use();
-    floor_shader.setInt("floor_texture", 0);
-
 #pragma endregion
 
 #pragma region box
 
+    float box_vertices[] = {
+        // back face
+        -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+         1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
+         1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 0.0f, // bottom-right         
+         1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
+        -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+        -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 1.0f, // top-left
+        // front face
+        -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
+         1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 0.0f, // bottom-right
+         1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
+         1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
+        -1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 1.0f, // top-left
+        -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
+        // left face
+        -1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
+        -1.0f,  1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-left
+        -1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
+        -1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
+        -1.0f, -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-right
+        -1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
+        // right face
+         1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
+         1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
+         1.0f,  1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-right         
+         1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
+         1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
+         1.0f, -1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-left     
+        // bottom face
+        -1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
+         1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 1.0f, // top-left
+         1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
+         1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
+        -1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 0.0f, // bottom-right
+        -1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
+        // top face
+        -1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
+         1.0f,  1.0f , 1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
+         1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 1.0f, // top-right     
+         1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
+        -1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
+        -1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 0.0f  // bottom-left        
+    };
 
+    GLuint box_VAO, box_VB0;
+    glGenVertexArrays(1, &box_VAO);
+    glGenBuffers(1, &box_VB0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, box_VB0);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(box_vertices), box_vertices, GL_STATIC_DRAW);
+
+    glBindVertexArray(box_VAO);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(0));
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 
 #pragma endregion
 
+#pragma region depth_texture
+
+    GLuint depthMapFBO;
+    glGenFramebuffers(1, &depthMapFBO);
+
+    const GLuint SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
+
+    GLuint depthMap;
+    glGenTextures(1, &depthMap);
+    glBindTexture(GL_TEXTURE_2D, depthMap);
+    
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+    GLfloat borderColor[] = { 1.0, 1.0, 1.0, 1.0 };
+    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+
+    glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
+    glDrawBuffer(GL_NONE);
+    glReadBuffer(GL_NONE);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    Shader depth_shader("shadowMapping/shaders/depth_vert.vs", "shadowMapping/shaders/depth_frag.fs");
+    
+#pragma endregion
+
+#pragma region debug_depth_quad
+
+    GLuint quad_VAO, quad_VBO;
+
+    float quad_vertex[] =
+    {
+        // positions        // texture Coords
+        -1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
+        -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+         1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
+         1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+    };
+
+    glGenVertexArrays(1, &quad_VAO);
+    glGenBuffers(1, &quad_VBO);
+
+    glBindVertexArray(quad_VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, quad_VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quad_vertex), quad_vertex, GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glBindVertexArray(0);
+
+    Shader debug_depth_shader("shadowMapping/shaders/debug_depth.vs", "shadowMapping/shaders/debug_depth.fs");
+
+    debug_depth_shader.setInt("depthMap", 0);
+
+#pragma endregion
+
+    Shader obj_shader("shadowMapping/shaders/obj_vert.vs", "shadowMapping/shaders/obj_frag.fs");
+
+    obj_shader.use();
+    obj_shader.setInt("diffuseTexture", 0);
+    obj_shader.setInt("shadowMap", 1);
+
+    //light
+    glm::vec3 lightPos = glm::vec3(-2.0f, 5.0f, -1.0f);
 
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
 
-    camera.setInitialStatus(glm::vec3(4.0f, 4.0f, 7.0f), glm::vec3(-0.3f, 0.8f, -0.5f), -120, -32);
+    camera.setInitialStatus(glm::vec3(4.0f, 4.0f, 7.0f), glm::vec3(-0.3f, 0.8f, -0.5f), -120, -32); 
 
     while (!glfwWindowShouldClose(window))
     {
@@ -101,22 +236,128 @@ void show_shadow_mapping(GLFWwindow* window, Camera& camera, unsigned int SCR_WI
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        floor_shader.use();
+        //--------------------depth map-------------------------
+        glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+        glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+        glClear(GL_DEPTH_BUFFER_BIT);
+
+        glCullFace(GL_FRONT);
+
+        GLfloat near_plane = 1.0f, far_plane = 7.5f;
+        glm::mat4 lightProjection = glm::ortho(-10.0f, 10.f, -10.f, 10.0f, near_plane, far_plane);
+        glm::mat4 lightView = glm::lookAt(lightPos, glm::vec3(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        glm::mat4 lightSpaceMatrix = lightProjection * lightView;
+
+        depth_shader.use();
+        depth_shader.setMat4("lightSpaceMatrix", lightSpaceMatrix);
         
+        //floor depth
         glm::mat4 model = glm::mat4(1.0);
-        floor_shader.setMat4("model", model);
+        depth_shader.setMat4("model", model);
+        glBindVertexArray(floor_VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        //box depth
+        glBindVertexArray(box_VAO);
+
+        model = glm::mat4(1.0);
+        model = glm::translate(model, glm::vec3(0.0f, 2.5f, 0.0f));
+        model = glm::scale(model, glm::vec3(0.5f));
+        depth_shader.setMat4("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(2.0f, 1.0f, 1.0f));
+        model = glm::scale(model, glm::vec3(0.5f));
+        depth_shader.setMat4("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(-1.0f, 1.0f, 2.0f));
+        model = glm::rotate(model, glm::radians(60.0f), glm::normalize(glm::vec3(1.0f, 0.0f, 1.0f)));
+        model = glm::scale(model, glm::vec3(0.25f));
+        depth_shader.setMat4("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+        glCullFace(GL_BACK);
+
+        //------------------debug depth map----------------------
+        //reset viewport
+        //glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+        //glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        //debug_depth_shader.use();
+        //debug_depth_shader.setFloat("near_plane", near_plane);
+        //debug_depth_shader.setFloat("far_plane", far_plane);
+        //glActiveTexture(GL_TEXTURE0);
+        //glBindTexture(GL_TEXTURE_2D, depthMap);
+        //
+        //glBindVertexArray(quad_VAO);
+        //glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+        //glBindVertexArray(0);
+        //------------------------------------------------------
+        
+        //----------------------floor---------------------------
+        glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        obj_shader.use();
+        
+        model = glm::mat4(1.0);
+        obj_shader.setMat4("model", model);
 
         glm::mat4 view = camera.GetViewMatrix();
-        floor_shader.setMat4("view", view);
+        obj_shader.setMat4("view", view);
 
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        floor_shader.setMat4("projection", projection);
+        obj_shader.setMat4("projection", projection);
+
+        obj_shader.setMat4("lightSpaceMatrix", lightSpaceMatrix);
+
+        //light info
+        obj_shader.setVec3("lightPos", lightPos);
+        obj_shader.setVec3("viewPos", camera.Position);
 
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, floor_texture);
 
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, depthMap);
+
         glBindVertexArray(floor_VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //-------------------------------------------------------
+
+        
+        //----------------------box------------------------------
+        obj_shader.use();
+
+        glBindVertexArray(box_VAO);
+
+        model = glm::mat4(1.0);
+        model = glm::translate(model, glm::vec3(0.0f, 2.5f, 0.0f));
+        model = glm::scale(model, glm::vec3(0.5f));
+        obj_shader.setMat4("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(2.0f, 1.0f, 1.0f));
+        model = glm::scale(model, glm::vec3(0.5f));
+        obj_shader.setMat4("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(-1.0f, 1.0f, 2.0f));
+        model = glm::rotate(model, glm::radians(60.0f), glm::normalize(glm::vec3(1.0f, 0.0f, 1.0f)));
+        model = glm::scale(model, glm::vec3(0.25f));
+        obj_shader.setMat4("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        glBindVertexArray(0);
+        //-------------------------------------------------------
+        
 
         glfwSwapBuffers(window);
         glfwPollEvents();
